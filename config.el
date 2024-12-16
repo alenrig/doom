0c78811f05241f34c341918cdb85cd431cd98b40
Aleksandr Gorokhov <agorokhov94@gmail.com>
2024-12-16 18:59:31 +0300

    Add markdown lang configs

--- config.el
+++ config.el
@@ -75,6 +75,9 @@
                           (require 'lsp-pyright)
                           (lsp))))  ; or lsp-deferred
 
+;; ----- Markdown -----
+(setq grip-preview-use-webkit t)
+
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
